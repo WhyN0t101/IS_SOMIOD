5a68b964021b0ab8204140493355e4f8795f3f24
Tiago Pereira <tpereira1911@gmail.com>
2023-12-07 16:15:16 +0000

    Esqueleto do projeto

--- Middleware/Models/Application.cs
+++ Middleware/Models/Application.cs
@@ -7,5 +7,9 @@ namespace Middleware.Models
 {
     public class Application
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Creation_dt { get; set; }
+        public string Res_type { get; set; }
     }
 }
