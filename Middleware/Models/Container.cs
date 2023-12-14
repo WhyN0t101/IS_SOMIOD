10017980d78b9ef8cacc9682bdf1e097d9e9e5b4
DiogoAndrade2002 <2211042@my.ipleiria.pt>
2023-12-14 16:47:56 +0000

    ContainerHandler(WIP)

--- Middleware/Models/Container.cs
+++ Middleware/Models/Container.cs
@@ -10,7 +10,9 @@ namespace Middleware.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Res_type { get; set; }
+        public DateTime Creation_dt { get; set; }
         public int Parent { get; set; }
+        public List<Data> Data { get; set; }
 
     }
 }
