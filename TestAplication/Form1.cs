6867b534b213bd58a0e1905f828b2e54601934e6
Tiago Pereira <tpereira1911@gmail.com>
2023-12-27 22:38:35 +0000

    Get/Post/Put/Delete App

--- TestAplication/Form1.cs
+++ TestAplication/Form1.cs
@@ -27,18 +27,103 @@ namespace TestAplication
         private void buttonGetAllApplications_Click(object sender, EventArgs e)
         {
             TextBoxListAllApplications.Clear();
-            string requestURI = "/api/somiod/";
+            string requestUri = "/api/somiod/";
 
-            // Use XDocument instead of XmlDocument
-            XDocument xDoc = RequestsHandler.getResponseAsXMLDocument(requestURI, client, "applications");
+            XDocument xDoc = RequestsHandler.getResponseAsXMLDocument(requestUri, client, "applications");
 
-            if (xDoc == null)
+            if (xDoc != null)
             {
+                TextBoxListAllApplications.Text = xDoc.ToString();
+            }
+        }
+
+        private void buttonGetApplication_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            string applicationName = textBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(applicationName))
+            {
+                MessageBox.Show("Please enter the application name");
                 return;
             }
 
-            // Loads the XML document to the RichTextBox
-            TextBoxListAllApplications.Text = xDoc.ToString();
+            string requestUri = $"/api/somiod/{applicationName}";
+            XDocument xDoc = RequestsHandler.getResponseAsXMLDocument(requestUri, client, "Application");
+
+            if (xDoc != null)
+            {
+                richTextBox1.Text = xDoc.ToString();
+            }
+        }
+
+        private void buttonPOSTApplication_Click(object sender, EventArgs e)
+        {
+            string applicationName = textBoxApplicationName.Text.Trim();
+
+            if (string.IsNullOrEmpty(applicationName))
+            {
+                MessageBox.Show("Please enter the application name");
+                return;
+            }
+
+            string requestUri = "/api/somiod/";
+
+            try
+            {
+                RequestsHandler.createApplication(requestUri, client, applicationName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void buttonPUTApplication_Click(object sender, EventArgs e)
+        {
+            string applicationName = textBoxApplicationName.Text.Trim();
+            string newApplicationName = textBoxApplicationNewName.Text.Trim();
+
+            if (string.IsNullOrEmpty(applicationName) || string.IsNullOrEmpty(newApplicationName))
+            {
+                MessageBox.Show("Please enter both application names");
+                return;
+        }
+
+            string requestUri = $"/api/somiod/{applicationName}";
+
+            try
+            {
+                RequestsHandler.updateApplicationName(requestUri, client, newApplicationName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void buttonDELApplication_Click(object sender, EventArgs e)
+        {
+            // Verifies if Application Name Input is Empty
+            string applicationName = textBoxApplicationName.Text.Trim();
+
+            if (string.IsNullOrEmpty(applicationName))
+            {
+                MessageBox.Show("Please enter the application name");
+                return;
+            }
+
+            // Makes the Delete Request
+            string requestUri = $"/api/somiod/{applicationName}";
+
+            try
+            {
+                RequestsHandler.Delete(requestUri, client);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
         }
 
     }
